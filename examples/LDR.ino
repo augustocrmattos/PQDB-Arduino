079ab7fa0f7c12708efaa868228012043b138264
projetopqdb <32346329+projetopqdb@users.noreply.github.com>
2017-10-04 11:39:55 -0300

    Create LDR.ino

---
+++ examples/LDR.ino
@@ -0,0 +1,26 @@
+#include "ad.h"
+#include "so.h"
+#include "ssd.h"
+#include "timer.h"
+
+void setup()
+{
+adInit();                              // Inicializa o adc
+soInit();                              // Inicializa o 74hc
+ssdInit();                             // Inicializa o Display de 7 Segmentos
+}
+
+int i;
+
+void loop()
+{
+    i  = adRead(1);                    // Leitura do LDR
+    ssdDigit(3, (i / 10) % 10);        // Dezena
+    ssdDigit(2, (i / 100) % 10);       // Unidade
+    ssdDigit(1,0);                     // 0
+    ssdDigit(0,0);                     // 0
+    for(i=0;i<8;i++){                  // Delay
+    ssdUpdate();
+    timerDelay(5);
+  }
+}
