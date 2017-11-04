b85fb031255c4ea44924619d7557c4c87414269a
projetopqdb <32346329+projetopqdb@users.noreply.github.com>
2017-11-04 09:57:19 -0200

    Add files via upload

---
+++ src/lcd.h
@@ -0,0 +1,9 @@
+#ifndef LCD
+  #define LCD
+  #include <Arduino.h>
+  void lcdCommand(char value);
+  void lcdChar(char value);
+  void lcdString(char msg[]);
+  void lcdCreateChar(int pos, byte line[]);
+  void lcdInit ();
+#endif
